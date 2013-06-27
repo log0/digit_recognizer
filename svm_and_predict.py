07c778bb0a56920957241ff2a80bcef48ca96b2f
Eric Chio <eric@bhotels.me>
2013-06-27 14:17:28 +0800

    Submission 4 pushed!

---
+++ svm_and_predict.py
@@ -0,0 +1,76 @@
+﻿"""
+This uses SVM
+
+Average accuracy at 97.667% on train.csv .
+
+D:\L\source\digit_recognizer>python svm_and_predict.py
+[C=       10000][gamma=0.0000000100000] Accuracy = 0.95980952381
+[C=       10000][gamma=0.0000001000000] Accuracy = 0.976666666667
+[C=       10000][gamma=0.0000010000000] Accuracy = 0.975904761905
+[C=      100000][gamma=0.0000000100000] Accuracy = 0.95980952381
+[C=      100000][gamma=0.0000001000000] Accuracy = 0.976666666667
+[C=      100000][gamma=0.0000010000000] Accuracy = 0.975904761905
+
+"""
+
+import csv
+import random
+from sklearn import svm
+
+train_file = 'data/train.csv'
+test_file = 'data/test.csv'
+
+test_data = [ i for i in csv.reader(file(test_file, 'rb')) ]
+test_data = test_data[1:] # remove header
+
+data = [ i for i in csv.reader(file(train_file, 'rb')) ]
+data = data[1:] # remove header
+random.shuffle(data)
+
+X = [ i[1:] for i in data ]
+Y = [ i[0] for i in data ]
+
+train_cutoff = len(data) * 3/4
+
+X_train = X[:train_cutoff]
+Y_train = Y[:train_cutoff]
+X_test = X[train_cutoff:]
+Y_test = Y[train_cutoff:]
+
+classifiers = []
+
+max_c = 0
+max_gamma = 0
+max_accuracy = 0
+max_classifier = None
+
+for c in [10000, 100000]:
+    for gamma in [0.00000001, 0.0000001, 0.000001]:
+        classifier = svm.SVC(C=c, gamma=gamma)        
+        classifier.fit(X_train, Y_train)
+
+        Y_predict = classifier.predict(X_test)
+
+        equal = 0
+        for i in xrange(len(Y_predict)):
+            if Y_predict[i] == Y_test[i]:
+                equal += 1
+
+        accuracy = float(equal)/len(Y_predict)
+        if accuracy > max_accuracy:
+            max_c = c
+            max_gamma = gamma
+            max_accuracy = accuracy
+            max_classifier = classifier
+        print '[C=%12d][gamma=%15.13f] Accuracy = %s' % (c, gamma, accuracy)
+        
+        classifiers.append(classifier)
+
+X_submission = [ i for i in test_data ]
+
+Y_submission = max_classifier.predict(X_submission)
+
+f = file('svm_and_predict.c%s.gamma%015.13f.out.csv' % (max_c, max_gamma), 'wb')
+f.write('\n'.join([str(i) for i in Y_submission]))
+f.close()
+    
---
+++ svm_cv_and_predict.py
@@ -0,0 +1,74 @@
+﻿"""
+This uses SVM with cross-validation using the parameters (C, gamma)=(10000, 0.0000001) and (100000, 0.0000001).
+
+Average accuracy at ??% on train.csv .
+
+"""
+
+import csv
+import random
+from sklearn import svm
+import numpy as np
+from sklearn.cross_validation import StratifiedKFold
+
+def get_accuracy(Y_predict, Y_test):
+    equal = 0
+    for i in xrange(len(Y_predict)):
+        if Y_predict[i] == Y_test[i]:
+            equal += 1
+
+    return float(equal)/len(Y_predict)
+
+train_file = 'data/train.csv'
+test_file = 'data/test.csv'
+
+test_data = [ i for i in csv.reader(file(test_file, 'rb')) ]
+test_data = test_data[1:] # remove header
+
+data = [ i for i in csv.reader(file(train_file, 'rb')) ]
+data = data[1:] # remove header
+
+X = np.array([ i[1:] for i in data ])
+Y = np.array([ i[0] for i in data ])
+K = 5
+
+skf = StratifiedKFold(Y, K)
+for i, (train_index_vector, test_index_vector) in enumerate(skf):
+    X_train = X[train_index_vector]
+    Y_train = Y[train_index_vector]
+    X_test = X[test_index_vector]
+    Y_test = Y[test_index_vector]
+    
+    classifiers = []
+
+    max_c = 0
+    max_gamma = 0
+    max_accuracy = 0
+    max_classifier = None
+
+    for c in [10000, 100000]:
+        for gamma in [0.0000001]:
+            classifier = svm.SVC(C=c, gamma=gamma)        
+            classifier.fit(X_train, Y_train)
+
+            Y_predict = classifier.predict(X_test)
+
+            accuracy = get_accuracy(Y_predict, Y_test)
+            
+            if accuracy > max_accuracy:
+                max_c = c
+                max_gamma = gamma
+                max_accuracy = accuracy
+                max_classifier = classifier
+            
+            print '[i=%d][C=%12d][gamma=%15.13f] Accuracy = %s' % (i, c, gamma, accuracy)
+            
+            classifiers.append(classifier)
+
+X_submission = [ i for i in test_data ]
+Y_submission = max_classifier.predict(X_submission)
+
+f = file('svm_and_predict.cross_validation.c%s.gamma%015.13f.out.csv' % (max_c, max_gamma), 'wb')
+f.write('\n'.join([str(i) for i in Y_submission]))
+f.close()
+    
